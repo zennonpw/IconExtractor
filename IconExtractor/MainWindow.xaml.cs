c4d9b628738a3fcafdcfbe3a4f9a6540f70ed300
Zennon <Zennon@ZENNON>
2024-06-16 00:54:25 -0300

    open output folder created

--- IconExtractor/MainWindow.xaml.cs
+++ IconExtractor/MainWindow.xaml.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.Win32;
+﻿using System;
 using System.Windows;
+using System.Windows.Forms;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 
 namespace IconExtractor
 {
     public partial class MainWindow : Window
     {
         private OpenFileDialog _ddsFile;
-        
+        private FolderBrowserDialog folderBrowser;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -15,13 +18,29 @@ namespace IconExtractor
         private void OpenFile()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "|*.dds";
+            openFileDialog.Filter = " |*.dds";
             openFileDialog.Title = "Select a .dds file";
             if (openFileDialog.ShowDialog() == true)
             {
                 _ddsFile = openFileDialog;
                 lblStatus.Content = "dds is ready to extract";
                 lblFilePath.Content = _ddsFile.FileName;
+            }
+        }
+
+        private void SelectOutputFolder()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                folderBrowser = folderBrowserDialog;
+                lblOutput.Content = folderBrowser.SelectedPath;
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Output folder error!");
             }
         }
 
@@ -37,7 +56,7 @@ namespace IconExtractor
 
         private void btnOutputFolder_Click(object sender, RoutedEventArgs e)
         {
-
+            SelectOutputFolder();
         }
     }
 }
