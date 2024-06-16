d84caa4b8e60cccfe6e3bbfbf910754f733a51b7
Zennon <Zennon@ZENNON>
2024-06-16 00:04:35 -0300

    open file created

--- IconExtractor/MainWindow.xaml.cs
+++ IconExtractor/MainWindow.xaml.cs
@@ -1,17 +1,33 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Windows;
 
 namespace IconExtractor
 {
     public partial class MainWindow : Window
     {
+        private OpenFileDialog _ddsFile;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void OpenFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "|*.dds";
+            openFileDialog.Title = "Select a .dds file";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _ddsFile = openFileDialog;
+                lblStatus.Content = "dds is ready to extract";
+                lblFilePath.Content = _ddsFile.FileName;
+            }
+        }
+
         private void btnOpenFile_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenFile();
         }
 
         private void btnExtract_Click(object sender, RoutedEventArgs e)
